f92bea6fd192a66b358bc834cae809b20fc47180
Dominik Walter <dominik_walter@epam.com>
2018-07-12 18:04:57 +0200

    Clean up pom

--- src/main/java/com/app/fuse/Route.java
+++ src/main/java/com/app/fuse/Route.java
@@ -3,7 +3,10 @@ package com.app.fuse;
 import org.apache.camel.builder.RouteBuilder;
 
 public class Route extends RouteBuilder {
-    public void configure() throws Exception {
-        from("cxfrs:bean:restService").process(new Processor());
+    public void configure() {
+        from("cxfrs:bean:restService").process(exchange -> {
+            String msg = exchange.getIn().getBody(String.class);
+            exchange.getOut().setBody("Hello World " + msg);
+        });
     }
 }
