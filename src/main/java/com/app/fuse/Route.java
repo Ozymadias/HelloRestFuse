fdfa6f6836e505f6c25abf447e2bb0abfc44e3da
Dominik Walter <dominik_walter@epam.com>
2018-07-12 17:37:00 +0200

    Inline processor

--- src/main/java/com/app/fuse/Route.java
+++ src/main/java/com/app/fuse/Route.java
@@ -3,7 +3,15 @@ package com.app.fuse;
 import org.apache.camel.builder.RouteBuilder;
 
 public class Route extends RouteBuilder {
-    public void configure() throws Exception {
-        from("cxfrs:bean:restService").process(new Processor());
+//    restConfiguration()
+//                .component("servlet")//todo: kto to robi pod spodem
+//                .host("0.0.0.0").port("8085")
+//                .bindingMode(RestBindingMode.json);
+
+    public void configure() {
+        from("cxfrs:bean:restService").process(exchange -> {
+            String msg = exchange.getIn().getBody(String.class);
+            exchange.getOut().setBody("Hello World " + msg);
+        });
     }
 }
